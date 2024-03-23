aa8594149ad23b916da2ab97cf1f24ac3ee7ccc9
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-03-23 21:50:55 +0100

    doc string is added in the unittest src code

--- unittest/unit-test.py
+++ unittest/unit-test.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import logging
-import argparse
 import unittest
 import torch
 
@@ -20,7 +19,16 @@ from src.config import RAW_PATH, PROCESSED_PATH
 
 
 class UnitTest(unittest.TestCase):
+    """
+    Defines a suite of unit tests for verifying the integrity and correctness of the
+    dataset loading functionality and dataset sizes for training and testing.
+    """
+
     def setUp(self):
+        """
+        Set up the test environment by loading the train and test dataloaders from
+        the PROCESSED_PATH.
+        """
         self.train_dataloader = load(
             os.path.join(PROCESSED_PATH, "train_dataloader.pkl")
         )
@@ -29,17 +37,31 @@ class UnitTest(unittest.TestCase):
         )
 
     def test_train_dataset_size(self):
+        """
+        Test that the dimensions of the data and label tensors in the training dataset
+        are as expected.
+        """
         data, label = next(iter(self.train_dataloader))
         self.assertEqual(data, torch.Size([4, 3, 256, 256]))
         self.assertEqual(label, torch.Size([4, 1, 256, 256]))
 
     def test_val_dataset_size(self):
+        """
+        Test that the dimensions of the data and label tensors in the validation dataset
+        are as expected.
+        """
         data, label = next(iter(self.test_dataloader))
         self.assertEqual(data, torch.Size([24, 3, 256, 256]))
         self.assertEqual(label, torch.Size([24, 1, 256, 256]))
 
     def test_quantity_train_dataset(self):
+        """
+        Test that the total number of samples in the training dataset is as expected.
+        """
         self.assertEqual(sum(data.size(0) for data in self.train_dataloader), 3256)
 
     def test_quantity_val_dataset(self):
+        """
+        Test that the total number of samples in the validation dataset is as expected.
+        """
         self.assertEqual(sum(data.size(0) for data in self.test_dataloader), 3256)
