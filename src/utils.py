8c547eca0980a5458fb7b7972712b7a04b0593c2
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-03-23 21:40:30 +0100

    doc string is added in the utils

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,16 @@ import joblib
 
 
 def dump(value=None, filename=None):
+    """
+    Serializes and saves a Python object to a file using Joblib.
+
+    Parameters:
+    - value: The Python object to serialize and save. Cannot be None.
+    - filename: The path to the file where the object should be saved. Cannot be None.
+
+    Raises:
+    - ValueError: If either 'value' or 'filename' is None.
+    """
     if value is not None and filename is not None:
         joblib.dump(value=value, filename=filename)
 
@@ -12,6 +22,18 @@ def dump(value=None, filename=None):
 
 
 def load(filename):
+    """
+    Loads and deserializes a Python object from a file using Joblib.
+
+    Parameters:
+    - filename: The path to the file from which the object should be loaded.
+
+    Returns:
+    - The deserialized Python object.
+
+    Raises:
+    - Exception: If the specified file does not exist.
+    """
     if os.path.exists(filename):
         return joblib.load(filename=filename)
     else:
